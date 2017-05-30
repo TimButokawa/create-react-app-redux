5b975243c78a591c341fbc0cf655131aeb62a91d
Tim Butorac <tim@timbutorac.com>
2017-05-30 11:37:45 -0700

    remove app files that have been refactored - update index to reflect changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import App from './App.container';
+import App from './App';
 import configureStore from './store';
 import registerServiceWorker from './utils/registerServiceWorker';
 import './index.css';
